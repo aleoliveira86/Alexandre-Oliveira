bea9c80205ae7e9972c423c82ce1b1f6b294b367
aleoliveira86 <54422709+aleoliveira86@users.noreply.github.com>
2019-08-22 22:07:02 -0300

    Add files via upload

---
+++ copia vetor2.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+main(){
+	
+	int v1[3],v2[3] i;
+	
+	for(i=0; i<3;i++){
+		printf("Digite os valores: ");
+		scanf("%d", &v[i]);
+	}
+	
+	for(i=0; i<3;i++){
+		v2[i]=v1[i];		
+	}
+	printf("\n\nVetor 2: \n\n");
+	for(i=0; i<3;i++){		
+		printf("%d ", v2[i]);
+	}	
+}
